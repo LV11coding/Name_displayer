63b24c0511e6bc522f70cfad4bf1b228acfcaf97
LV11coding <94009217+LV11coding@users.noreply.github.com>
2024-05-29 20:15:14 +0300

    Add files via upload

---
+++ tishofile.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <string>
+#include <windows.h>
+
+using namespace std;
+
+int main()
+{
+    cout << "Please enter your first name: ";
+
+    string name;
+    cin >> name;
+
+    const string greeting = "Hello, " + name + "!";
+
+    const string::size_type colls = greeting.size() + 2; //int
+    const string star((greeting.size() - name.size())/2, '.');
+    int r = 0;
+    string::size_type c = 0;
+    int rows = 11;
+    bool sup = true;
+
+    cout << endl;
+
+    r = 0;
+    while (r != rows)
+    {
+        c = 0;
+        while (c != colls)
+        {
+            if (r == 0 || c == 0 || c == colls - 1 || r == rows - 1)
+            {
+                cout << "*";
+            }
+            else if(r == (rows-1)/2 && c == 1){
+                cout<<star<<name<<star;
+                sup = false;
+            }
+            else if (sup == true)
+            {
+                cout << ".";
+            }
+            c++;
+        }
+        sup = true;
+        cout << endl;
+        r++;
+    }
+    Sleep(2000);
+    return 0;
+}
